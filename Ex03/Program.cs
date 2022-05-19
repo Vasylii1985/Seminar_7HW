e642b50ad0182b82cd16d57c326b4570e04c1b49
Vasilii <bespalchuk1985@gmail.com>
2022-05-19 14:22:24 +0300

    Ex03

--- Ex03/Program.cs
+++ Ex03/Program.cs
@@ -1,11 +1,17 @@
-﻿Console.WriteLine("Введите количество строк:");
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3*/
+
+Console.WriteLine("Введите количество строк:");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов:");
 int m = Convert.ToInt32(Console.ReadLine());
 int[,] array = new int[n, m];
-double[] summ = new double[n];
-
-for (int i = 0; i < n; i++)
+double[] summ = new double[m];
+for (int i = 0; i < n; i++) 
 {
     for (int j = 0; j < m; j++) 
     {
@@ -13,19 +19,18 @@ for (int i = 0; i < n; i++)
         Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
-}   
 
-for (int i = 0; i < n; i++) 
+}
+for (int i = 0; i < m; i++) 
 {
-    for (int j = 0; j < m; j++) 
+    for (int j = 0; j < n; j++) 
     {
-        summ[i] = summ[i] + array[i, j]; 
+        summ[i] = summ[i] + array[j, i]; 
     }
 }
-Console.Write($"Среднее арифметическое элементов в каждой строке: ");
-
-for (int i = 0; i < n; i++)
+Console.Write($"Среднее арифметическое элементов в каждом столбце: ");
+for (int i = 0; i < m; i++)
 {
-    Console.Write($"{Math.Round(summ[i] / m,1)} ");
+    Console.Write($" {Math.Round(summ[i] / n,1)}; ");
 }
 Console.WriteLine();
